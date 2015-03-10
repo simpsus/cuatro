7e841dc864ed0725844ba22db862a366fce81877
Bastian Kennel <bastian.kennel@gmail.com>
2015-03-10 18:06:46 +0100

    merge preparation

--- probability.py
+++ probability.py
@@ -9,31 +9,33 @@ class DiceAnalyzer():
 		free = 5 - initial
 		result = 0
 		# the first option is to hit all now in this throw
-		result += (1./6 ** free)
+		result += ((1./6) ** free)
 		# if this is the first dice, then actually you will always hit
 		if initial == 0:
 			result *= 6
 		# if no more options are left, than that's it
 		if left == 0:
 			return result
-		# there is still hope == throws left
+		# there is still hope == throws 
+		# eaxmine what can happen in this throw and branch
 		if free > 4:
 			# if we have all dices free, we can have 4 matches now and still go on
-			result += (1./6 ** 4) * (5./6 ** 1) * 5 * self.yathzee(initial + 4, left - 1)
+			result += ((1./6) ** 4) * ((5./6) ** 1) * 5 * self.yathzee(initial + 4, left - 1)
 		if free > 3:
 			# with 4 dices, we can draw three now and go on
-			result += (1./6 ** 3) * (5./6 ** (free-3)) * out_of(free, 3) * self.yathzee(initial + 3, left - 1)
+			result += ((1./6) ** 3) * ((5./6) ** (free-3)) * out_of(free, 3) * self.yathzee(initial + 3, left - 1)
 		if free > 2:
 			# two matches possible with 3 dices
-			result += (1./6 ** 2) * (5./6 ** (free-2)) * out_of(free, 2) * self.yathzee(initial + 2, left - 1)
+			result += ((1./6) ** 2) * ((5./6) ** (free-2)) * out_of(free, 2) * self.yathzee(initial + 2, left - 1)
 		if free > 1:  	
 			# we can have one match
-			result += 1./6 * (5./6 ** (free-1)) * free * self.yathzee(initial + 1, left - 1)
+			result += 1./6 * ((5./6) ** (free-1)) * free * self.yathzee(initial + 1, left - 1)
 		# any given sunday, we can have no luck
 		result += 5./6 ** free * self.yathzee(initial, left - 1)
 		return result
 
-	#def full_house(self, initial=[], left=4)
+	def full_house(self, dice):
+		
 
 def is_yathzee(faces):
 	face = faces[0]
@@ -78,6 +80,6 @@ def simulate_yathzee():
 
 
 if __name__ == "__main__":
-	#da = DiceAnalyzer()
-	#print da.yathzee(0,1)
-	simulate_yathzee()
+	da = DiceAnalyzer()
+	print da.yathzee()
+	#simulate_yathzee()
